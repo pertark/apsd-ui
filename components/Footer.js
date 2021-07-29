591e41b6b09c4e946ebcbd362273c196801e32b8
Patrick Zhang <zhangpatrick2004@gmail.com>
2021-07-29 16:53:48 -0400

    a

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+import { Box, Button, Flex, Heading, Stack, Slide, LinkGrid, StackDivider } from "@chakra-ui/react"
+import { HamburgerIcon, StarIcon } from "@chakra-ui/icons";
+
+export default function Footer(props) {
+  return (
+    <Box 
+      as="footer" 
+      role="contentinfo" 
+      mx="auto" 
+      // maxW="7xl" 
+      py="12" 
+      px={{ base: '4', md: '8' }}
+      bg="ap.200"
+      width="100%"
+      textColor="white"
+    >
+      insert footer content here
+    </Box>
+  );
+};
