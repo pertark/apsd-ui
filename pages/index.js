89c01eeb780a27e8fd9d50e778dd4aa6067dd740
ra <davidchen0@protonmail.com>
2021-07-29 21:49:07 -0400

    toast message for auth error

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,37 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Layout from '../components/Layout'
+import Cookies from 'js-cookie'
+import { useToast } from "@chakra-ui/react"
+import { Component, useEffect } from 'react'
 
 export default function Home() {
+  let toast = useToast()
+  let body = "test"
+
+  useEffect(() => {
+    // append message to body to be popped up as a toast
+    let message = JSON.parse(Cookies.get('message') || "{}");
+    console.log(message)
+    if (message !== {}) {
+      if (message.name === "error") {
+        window.onload = toast({
+          title: "Error",
+          description: message.message,
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
+    Cookies.remove('message');
+  })
+
   return (
     <Layout>
-    <p>sample text</p>
-
-    
+    <p>
+      {body}
+    </p>
     </Layout>
   )
 }
