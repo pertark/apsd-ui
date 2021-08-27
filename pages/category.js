3968576fa511f2357c832618e9931eaabdac0fd4
Patrick Zhang <zhangpatrick2004@gmail.com>
2021-08-27 12:26:09 -0400

    eslint stupid

--- pages/category.js
+++ pages/category.js
@@ -1,15 +1,15 @@
 import Layout from '../components/Layout'
-import { Button, Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { useState, useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
-import Exam from '../components/Exam'
+// import Exam from '../components/Exam'
 import UserContext from '../components/UserContext'
 
 export default function Category () {
-  const { authed, setAuth } = useContext(UserContext)
+  const { authed } = useContext(UserContext)
 
   const [exams, setExams] = useState([])
-  const [body, setBody] = useState("")
+  const [body, setBody] = useState('')
 
   const router = useRouter()
 
@@ -18,7 +18,7 @@ export default function Category () {
     const uuid = router.query.uuid
     if (uuid) {
       fetch(`/api/category/${uuid}`).then(async (response) => {
-        let json = await response.json()
+        const json = await response.json()
         console.log(json)
         setExams(json)
       })
@@ -33,18 +33,18 @@ export default function Category () {
   )
 
   console.log(exams.length)
-  //const authedBody = !!exams ? (exams.length !== 0 ? exams.map((val, idx) => { return (<Exam uuid={val} key={idx}></Exam>) }) : <Box>No exams in this category!!</Box>) : (<Box>loading...</Box>)
-  
-  if ( exams ) {
-    if ( exams.length == 0 ) {
+  // const authedBody = !!exams ? (exams.length !== 0 ? exams.map((val, idx) => { return (<Exam uuid={val} key={idx}></Exam>) }) : <Box>No exams in this category!!</Box>) : (<Box>loading...</Box>)
+
+  if (exams) {
+    if (exams.length === 0) {
       setBody(<Box>No Exams</Box>)
     } else {
-      setBody(exams.map((val, idx) => <Box>placeholder {idx}, val {val}</Box>))
+      setBody(exams.map((val, idx) => <Box key={idx}>placeholder {idx}, val {val}</Box>))
     }
   } else {
     setBody(<Box>Loading...</Box>)
   }
-  
+
   return (
     <Layout authed={authed}>
       {authed ? body : unauthedBody}
